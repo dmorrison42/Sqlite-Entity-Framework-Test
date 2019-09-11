c44d902f74367bbb2a3d5ef493b8035b88f15bd2
Daniel Morrison <dan@morrison.ph>
2019-09-10 20:32:45 -0400

    Add Description and test code

--- Model.cs
+++ Model.cs
@@ -18,6 +18,7 @@ namespace ConsoleApp.SQLite
     {
         public int BlogId { get; set; }
         public string Url { get; set; }
+        public string Description { get; set; }
 
         public ICollection<Post> Posts { get; set; }
     }
